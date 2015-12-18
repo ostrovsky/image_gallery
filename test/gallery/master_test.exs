e5aa468958dd429c5b121cb3d565adb3acf7d6fe
Karol Ostrovsky <karol.ostrovsky@gmail.com>
2015-12-18 12:03:57 +0100

    Sligthly improved master test

--- test/gallery/master_test.exs
+++ test/gallery/master_test.exs
@@ -7,7 +7,6 @@ defmodule Gallery.MasterTest do
   test "Master updates keys once a minute" do
     r_key = :base64.encode(:crypto.strong_rand_bytes(64))
     s_key = :base64.encode(:crypto.strong_rand_bytes(64))
-    {:ok, old_recipients, old_senders} = Gallery.Master.get_keys
     mail = %Mail{recipient: r_key,
                  sender: s_key,
                  subject: "",
@@ -16,15 +15,19 @@ defmodule Gallery.MasterTest do
     {:ok, old_interval} = GenServer.call(Gallery.Master,
                                          {:set_update_interval, 100},
                                          :infinity)
+    {:ok, old_recipients, old_senders} = Gallery.Master.get_keys
     Amnesia.transaction do: Mail.write(mail)
     :timer.sleep(200)
-    {:ok, new_recipients, new_senders} = Gallery.Master.get_keys
+    {:ok, add_recipients, add_senders} = Gallery.Master.get_keys
     Amnesia.transaction do: Mail.delete(r_key)
     :timer.sleep(200)
+    {:ok, del_recipients, del_senders} = Gallery.Master.get_keys
     {:ok, 100} = GenServer.call(Gallery.Master,
                                 {:set_update_interval, old_interval},
                                 :infinity)
-    assert Enum.member?(new_recipients -- old_recipients, r_key)
-    assert Enum.member?(new_senders -- old_senders, s_key)
+    assert Enum.member?(add_recipients -- old_recipients, r_key)
+    assert Enum.member?(add_senders -- old_senders, s_key)
+    refute Enum.member?(del_recipients, r_key)
+    refute Enum.member?(del_senders, s_key)
   end
 end
